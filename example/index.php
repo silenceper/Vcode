ff84ca6f3d0c1a134c81cd99ec1853bc1a5658f8
silenceper <silenceper@gmail.com>
2013-07-02 18:41:41 +0800

    add some note

--- example/index.php
+++ example/index.php
@@ -1,6 +1,9 @@
 <?php
+	session_start();
 	include '../Vcode.class.php';
 	$code=new Vcode();
 	//参数true表示 使用中文验证码  默认使用英文验证码
 	$code->showImage("./simsun.ttc",true);
+	//获取session  必须在 showImage 方法之后再获取验证码字符串
+	$_SESSION['code']=$code->code; 
 ?>
